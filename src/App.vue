de79b85a3e7fff9071419508a7254eca708d2cdc
Hasin Hayder <hasin@leevio.com>
2023-07-13 14:46:46 +0600

    Code Format

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,15 @@ const weather = "r"
     <h1 class="text-center text-2xl py-10">Logic in Vue.js</h1>
     <div class="w-1/3 bg-gray-200 p-10 flex items-center flex-col space-y-10">
       <h1 class="text-3xl">{{ location }}</h1>
-      <h2 class="text-2xl" v-show="weather=='s'">Weather: Sunny</h2>
-      <h2 class="text-2xl" v-show="weather=='pc'">Weather: Partly Cloudy</h2>
-      <h2 class="text-2xl" v-show="weather=='r'">Weather: Rainy</h2>
-      <h2 class="text-2xl" v-show="weather=='t'">Weather: Thunderstorm</h2>
-      <h2 class="text-2xl" v-show="weather=='c'">Weather: Cyclone</h2>
-      <img v-show="weather=='s'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7911203/weather-icon-md.png" alt="">
-      <img v-show="weather=='pc'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912718/weather-icon-md.png" alt="">
-      <img v-show="weather=='r'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7913380/weather-icon-md.png" alt="">
-      <img v-show="weather=='t'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912589/weather-icon-md.png" alt="">
+      <h2 class="text-2xl" v-show="weather == 's'">Weather: Sunny</h2>
+      <h2 class="text-2xl" v-show="weather == 'pc'">Weather: Partly Cloudy</h2>
+      <h2 class="text-2xl" v-show="weather == 'r'">Weather: Rainy</h2>
+      <h2 class="text-2xl" v-show="weather == 't'">Weather: Thunderstorm</h2>
+      <h2 class="text-2xl" v-show="weather == 'c'">Weather: Cyclone</h2>
+      <img v-show="weather == 's'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7911203/weather-icon-md.png" alt="">
+      <img v-show="weather == 'pc'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912718/weather-icon-md.png" alt="">
+      <img v-show="weather == 'r'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7913380/weather-icon-md.png" alt="">
+      <img v-show="weather == 't'" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912589/weather-icon-md.png" alt="">
     </div>
   </section>
 </template>
